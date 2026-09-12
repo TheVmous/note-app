3701979c154cdfdb403c4414d72dfc63973f00d5
Vmous <vedjoshi2007@gmail.com>
2026-09-11 23:07:46 -0400

    feat: added default file extension to config

--- src/config.rs
+++ src/config.rs
@@ -3,24 +3,46 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::print;
 
 // TODO: add 'separate structs' for like packages style stuff
 
 
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct VisualConfigs {
-    pub font_size: f32,
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub visuals: VisualConfigs,
+    pub syst: SystConfigs,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SystConfigs {
+    pub default_ext: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct VisualConfigs {
+    pub font_size: f32,
+    pub tab_len: i32,
+}
+
+/* end config structs */
 
 impl Default for VisualConfigs {
     fn default() -> Self {
-        Self { font_size: 12.0 }
+        Self {
+            font_size: 12.0,
+            tab_len: 4,
+        }
+    }
+}
+
+impl Default for SystConfigs {
+    fn default() -> Self {
+        Self {
+            default_ext: String::from(".md"),
+        }
     }
 }
 
@@ -28,6 +50,7 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             visuals: VisualConfigs::default(),
+            syst: SystConfigs::default(),
         }
     }
 }
@@ -45,5 +68,6 @@ pub fn read_config() -> Result<Config> {
     }
     let content: String = std::fs::read_to_string("config.toml")?;
     let config: Config = toml::from_str(&content)?;
+    print!("{}", config.syst.default_ext);
     Ok(config)
 }
